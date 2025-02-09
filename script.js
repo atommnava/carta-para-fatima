b3192d2007971e3303a4d3e68c85eb264f249497
Atom M. Nava <atommunoznava@gmail.com>
2025-02-09 17:12:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function selectOption(option) {
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
         document.getElementById('no-button').innerText = 'Vamos pastelito :('; 
+        document.getElementById('no-button').innerText = '1'; 
+        document.getElementById('no-button').innerText = '2'; 
+        document.getElementById('no-button').innerText = '3'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
